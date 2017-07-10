d3a649b3c94b1b6e475597b5d55e0981dc094979
Jay Deverett <jayjdeverett@gmail.com>
2017-07-10 00:06:51 -0400

    add bugged first draft - method error

--- farm.rb
+++ farm.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 class Farm
 
   def initialize(name)
     puts "Welcome to #{name}!"
     @name = name
+    main_menu
   end
 
   def main_menu
@@ -14,35 +17,128 @@ class Farm
   end
 
   def print_main_menu
-    print 'What would you like to do?'
-    print '[1] > add a new field'
-    print '[2] > harvest crops'
-    print '[3] > check the status of a field'
-    print '[4] > take a break and relax'
-    print '[5] > exit the program'
+    puts 'What would you like to do?'
+    puts '[1] > add a new field'
+    puts '[2] > harvest crops'
+    puts '[3] > check the status of your farm'
+    puts '[4] > take a break and relax'
+    puts '[5] > exit the program'
   end
 
   def call_option(user_selected)
     case user_selected
-    when 1 then field
+    when 1 then add_field
     when 2 then harvest
     when 3 then status
     when 4 then relax
-    when 5 then main_menu = false
-      puts "goodbye!"
+    when 5 then puts "goodbye!"
+      exit
     end
   end
 
+  def add_field
+    puts "What type of field are you planting: Corn or Wheat?"
+    field_type = gets.chomp.upcase
+    puts "How many Hectares will this field be?"
+    field_size = gets.chomp.to_i
+
+    if field_type == 'CORN'
+      Corn.create('CORN', field_size)
+    elsif field_type == 'WHEAT'
+      Wheat.create('WHEAT', field_size)
+    else
+      puts 'Sorry, that is not a valid entry'
+      main_menu
+    end
+    puts "Added a #{field_type} field of #{field_size} hectares!"
+  end
+
+  def harvest
+    Field.harvest
+  end
+
+  def status
+    Field.status
+  end
+
+  def relax
+    puts "You sit back and enjoy your farm. What a lovely day!"
+    puts ""
+    main_menu
+  end
+
+end
+
+class Field
+
+    @@fields = []
+    @@total_yield = 0
+
+    attr_accessor :field_size, :yield
+
+    CORN = 20
+    WHEAT = 30
+
+    def initialize(field_type, field_size)
+      @field_type = field_type
+      @field_size = field_size
+    end
+
+    def self.create(field_type, field_size)
+      field = self.new(field_type, field_size)
+      @@fields << field
+    end
+
+    def self.harvest
+      total = 0
+      @@fields.each do |field|
+        total += field.yield
+        puts "Harvesting #{total} tons of food from a #{field.field_size} hectare field."
+      end
+      @@total_yield = total + field.size * field.yield
+
+      puts "The farm has now harvested #{self.total_yield} tons of food in total"
+
+    end
+
+    def self.total_yield
+      total = 0
+      @@fields.each do |field|
+        total += field.yield
+      end
+      puts "The farm has harvested #{total} tons of food in total."
+    end
+
+    def self.status
+      @@fields.each do |field|
+      puts "#{field.field_type} field is #{field.field_size} hectares."
+    end
+    end
 
 
 end
 
 
-class Corn
+class Corn < Field
+
+  def initialize(field_type, field_size)
+    super(field_type, field_size)
+    @yield = field_size * CORN
+  end
 
 end
 
 
-class Wheat
+class Wheat < Field
+
+  def initialize(field_type, field_size)
+    binding.pry
+    super(field_type, field_size)
+    @field_type = field_type
+    @field_size = field_size
+    @yield = field_size * WHEAT
+  end
 
 end
+
+a = Farm.new("Jays farm")
